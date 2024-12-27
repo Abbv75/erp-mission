2972ca8b63758c2925f7be47f301d695915cd9c3
abbv75 <bore.younous59@gmail.com>
2024-12-27 13:46:08 +0000

    a

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/inter';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
