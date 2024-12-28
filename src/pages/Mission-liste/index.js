3afbd513464cb54b4dbc9f7144e22a3c8f2ee256
abbv75 <bore.younous59@gmail.com>
2024-12-28 16:38:59 +0000

    `Removed unused imports and components, simplified MissionListe component`

--- src/pages/Mission-liste/index.js
+++ src/pages/Mission-liste/index.js
@@ -1,7 +1,8 @@
-import { faFeatherAlt, faPlus, faTrashArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Avatar, Button, ButtonGroup, Card, Divider, Stack, Typography } from '@mui/joy'
+import { Avatar, Button, Stack, Typography } from '@mui/joy'
 import React from 'react'
+import MissionItem from './MissionItem'
 
 const MissionListe = () => {
     const handleAdd = () => {
@@ -35,67 +36,7 @@ const MissionListe = () => {
                 direction={"row"}
                 gap={2}
             >
-                <Card
-                    sx={{
-                        p: 1
-                    }}
-                >
-                    <Stack
-                        sx={{
-                            flexDirection: "row",
-                            alignItems: "center",
-                            gap: 2
-                        }}
-                    >
-
-                        <Avatar variant='outlined' color='primary' sx={{ fontWeight: 700 }}>
-                            1
-                        </Avatar>
-
-                        <Stack
-                            sx={{
-                                "&>div": {
-                                    flexDirection: "row",
-                                    gap: 1
-                                }
-                            }}
-                        >
-                            <Stack>
-                                <Typography fontWeight={700} color='primary'>Date de depart:</Typography>
-                                <Typography>5/845/4</Typography>
-                            </Stack>
-                            <Divider />
-
-                            <Stack>
-                                <Typography fontWeight={700} color='primary'>Date d'arrivée:</Typography>
-                                <Typography>5/845/4</Typography>
-                            </Stack>
-                            <Divider />
-
-                            <Stack>
-                                <Typography fontWeight={700} color='primary'>Distance:</Typography>
-                                <Typography>500 KM</Typography>
-                            </Stack>
-                        </Stack>
-                    </Stack>
-
-                    <ButtonGroup variant='solid'>
-                        <Button
-                            fullWidth
-                            color='danger'
-                            startDecorator={
-                                <FontAwesomeIcon icon={faTrashArrowUp} />
-                            }
-                        >Supprimer</Button>
-                        <Button
-                            fullWidth
-                            color='primary'
-                            endDecorator={
-                                <FontAwesomeIcon icon={faFeatherAlt} />
-                            }
-                        >Modifier</Button>
-                    </ButtonGroup>
-                </Card>
+                <MissionItem/>
             </Stack>
         </Stack>
     )
