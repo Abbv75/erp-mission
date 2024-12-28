eef3f7ec78e68a6943d7a6d114ce3cf5ff755aee
abbv75 <bore.younous59@gmail.com>
2024-12-28 15:57:20 +0000

    addVehicule

---
+++ BackEnd/API/addVehicule.php
@@ -0,0 +1,46 @@
+<?php
+require "../scripts/CORS.php";
+require "../scripts/connexion_bd.php";
+require "../classes/Response.php";
+
+$response = new Response(null, 200, []);
+
+try {
+    if (!isset(
+        $_POST['matricule'],
+        $_POST['date_achat'],
+        $_POST['type'],
+        $_POST['marque']
+    )) {
+        $response->satutCode403();
+    }
+
+    extract($_POST);
+
+    $query = $bdd->prepare('SELECT * FROM vehicule WHERE matricule=?');
+    $query->execute([$matricule]);
+
+    if ($query->fetch()) {
+        $response->satutCode403("Une voiture existe deja avec ce login");
+    }
+
+    $query = $bdd->prepare('INSERT INTO vehicule (
+        matricule, 
+        date_achat, 
+        type, 
+        marque, 
+    ) VALUES (?,?,?,?) ');
+
+    if (!$query->execute([
+        $matricule,
+        $date_achat,
+        $type,
+        $marque,
+    ])) {
+        $response->satutCode500("sauvegarde echouer");
+    }
+
+    $response->satutCode200();
+} catch (\Throwable $th) {
+    $response->satutCode500();
+}
